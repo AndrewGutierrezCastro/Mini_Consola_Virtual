2e5db58afe363ca16d931a7cfd211bb93348f4cd
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-12-13 11:20:53 -0600

    Cambios para colocar imagenes en el tablero.

--- src/pantalla/pantalla/modelo/Casilla.java
+++ src/pantalla/pantalla/modelo/Casilla.java
@@ -1,14 +1,25 @@
 package pantalla.modelo;
+import javax.swing.ImageIcon;
 
 public class Casilla{
 	public int posicionX, posicionY;
 	public int color;
+	public String nombreImagen;
 	public Casilla(int posicionX, int posicionY, int color) {
 		super();
 		this.posicionX = posicionX;
 		this.posicionY = posicionY;
 		this.color = color;
 	}
+	
+	public Casilla(int posicionX, int posicionY, int color, String nombreImagen) {
+		super();
+		this.posicionX = posicionX;
+		this.posicionY = posicionY;
+		this.color = color;
+		this.nombreImagen = nombreImagen;
+	}
+
 	@Override
 	public String toString() {
 		return "Casilla [posicionX=" + posicionX + ", posicionY=" + posicionY + ", color=" + color + "]";
